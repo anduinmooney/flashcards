56fc5e4b0db12968a41127a1751c0b4c31ab24ca
Anduin Mooney and Bella Vanillo <bella@>
2017-11-28 15:19:07 -0800

    Create a website that uses javascript for flashcards.

---
+++ js/scripts.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+
+  $(".flashcard2").click(function() {
+      $(".flashcard2 .well p").fadeToggle();
+    });
+
+  $(".flashcard3").click(function() {
+      $(".flashcard3 .well p").fadeToggle();
+    });
+
+  $(".flashcard4").click(function() {
+      $(".flashcard4 .well p").fadeToggle();
+    });
+
+  $(".flashcard5").click(function() {
+      $(".flashcard5 .well p").fadeToggle();
+    });
+
+  $(".flashcard6").click(function() {
+      $(".flashcard6 .well p").fadeToggle();
+    });
+
+  $(".flashcard7").click(function() {
+      $(".flashcard7 .well p").fadeToggle();
+    });
+
+  $(".flashcard8").click(function() {
+      $(".flashcard8 .well p").fadeToggle();
+    });
+
+  $(".flashcard9").click(function() {
+      $(".flashcard9 .well p").fadeToggle();
+    });
+
+  $(".flashcard10").click(function() {
+      $(".flashcard10 .well p").fadeToggle();
+    });
+
+  $(".flashcard11").click(function() {
+      $(".flashcard11 .well p").fadeToggle();
+    });
+
+  $(".flashcard12").click(function() {
+      $(".flashcard12 .well p").fadeToggle();
+    });
+
+  $(".flashcard13").click(function() {
+      $(".flashcard13 .well p").fadeToggle();
+    });
+
+  $(".flashcard14").click(function() {
+      $(".flashcard14 .well p").fadeToggle();
+    });
+
+  $(".flashcard15").click(function() {
+      $(".flashcard15 .well p").fadeToggle();
+    });
+
+  $(".flashcard16").click(function() {
+      $(".flashcard16 .well p").fadeToggle();
+    });
+
+  $(".flashcard17").click(function() {
+      $(".flashcard17 .well p").fadeToggle();
+    });
+
+  $(".flashcard18").click(function() {
+      $(".flashcard18 .well p").fadeToggle();
+    });
+
+  $(".flashcard19").click(function() {
+      $(".flashcard19 .well p").fadeToggle();
+    });
+
+  $(".flashcard20").click(function() {
+      $(".flashcard20 .well p").fadeToggle();
+    });
+
+});
